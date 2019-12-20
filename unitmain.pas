1ce55c04f113ba8111fff4bf8a9b5de603add588
mozturkgss <mozturkgss@gmail.com>
2019-12-20 22:06:58 +0300

    Add files via upload

--- unitmain.pas
+++ unitmain.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Buttons,
-  ComCtrls, UTF8Process, Process;
+  ComCtrls, UTF8Process, Process, FileUtil;
 
 type
   TFormMain = class(TForm)
@@ -24,7 +24,7 @@ type
     procedure ButtonStartClick(Sender: TObject);
     procedure FormDropFiles(Sender: TObject; const FileNames: array of String);
   private
-    procedure StartWin(FileName: String; OverWrite: Boolean; Dpi: String);
+    procedure Start(FileName: String; OverWrite: Boolean; Dpi: String);
   public
   end;
 
@@ -60,10 +60,10 @@ end;
 
 procedure TFormMain.ButtonStartClick(Sender: TObject);
 var
-	I: Integer;
+  I: Integer;
   Dpi: String;
 begin
-	Case CBQuality.ItemIndex of
+  Case CBQuality.ItemIndex of
   	0 : Dpi := '600';
     1 : Dpi := '300';
     2 : Dpi := '150';
@@ -71,36 +71,63 @@ begin
   end;
   ProgressBarProcess.Position := 0;
   ProgressBarProcess.Max := ListBoxFileNames.Items.Count;
-	For I:=0 to ListBoxFileNames.Items.Count-1 do
+  For I:=0 to ListBoxFileNames.Items.Count-1 do
   begin
-    {$IFDEF WINDOWS}
-    	StartWin(ListBoxFileNames.Items.Strings[I], CBOverride.Checked, Dpi);
-    {$ENDIF}
-    {$IFDEF LINUX}
-
-    {$ENDIF}
+    Start(ListBoxFileNames.Items.Strings[I], CBOverride.Checked, Dpi);
     ProgressBarProcess.Position := ProgressBarProcess.Position + 1;
   end;
   ShowMessage('Success');
   ListBoxFileNames.Clear;
 end;
 
-procedure TFormMain.StartWin(FileName: String; OverWrite: Boolean; Dpi: String);
+procedure TFormMain.Start(FileName: String; OverWrite: Boolean; Dpi: String);
 var
   AProcess: TProcessUTF8;
   NewFileName: String;
 begin
   AProcess := TProcessUTF8.Create(nil);
-  AProcess.Executable:= 'gs\bin\gswin32c.exe';
+  {$IFDEF WIN32}
+    AProcess.Executable:= 'gs32\bin\gswin32c.exe';
+  {$ENDIF}
+  {$IFDEF WIN64}
+    AProcess.Executable:= 'gs64\bin\gswin64c.exe';
+  {$ENDIF}
+  {$IFDEF LINUX}
+    AProcess.Executable:= FindDefaultExecutablePath('gs');
+  {$ENDIF}
   NewFileName := StringReplace(FileName, '.pdf', 'New.pdf', [rfReplaceAll, rfIgnoreCase]);
-  AProcess.Executable := AProcess.Executable + ' -sOutputFile="' + NewFileName + '"';
-  AProcess.Executable := AProcess.Executable + ' -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/default -dEmbedAllFonts=true -dAutoRotatePages=/PageByPage -dParseDSCComments=false -sColorConversionStrategy=RGB -dProcessColorModel=/DeviceRGB -dConvertCMYKImagesToRGB=true -dAutoFilterColorImages=false -dAutoFilterGrayImages=false -dEncodeColorImages=true -dEncodeGrayImages=true -dColorImageFilter=/DCTEncode -dGrayImageFilter=/DCTEncode -dDownsampleColorImages=true -dColorImageResolution='+Dpi+' -dDownsampleGrayImages=true -dGrayImageResolution='+Dpi+' -dEncodeMonoImages=true -dMonoImageFilter=/CCITTFaxEncode -c ".setpdfwrite << /ColorImageDict <</QFactor 2.4 /Blend 1 /HSample [2 1 1 2] /VSample [2 1 1 2]>> >> setdistillerparams" ".setpdfwrite << /GrayImageDict <</QFactor 2.4 /Blend 1 /HSample [2 1 1 2] /VSample [2 1 1 2]>> >> setdistillerparams"';
-  AProcess.Executable := AProcess.Executable + ' -f "' + FileName + '"';
+  AProcess.Parameters.Add('-sOutputFile="' + NewFileName + '"');
+  AProcess.Parameters.Add('-dNOPAUSE');
+  AProcess.Parameters.Add('-dBATCH');
+  AProcess.Parameters.Add('-sDEVICE=pdfwrite');
+  AProcess.Parameters.Add('-dCompatibilityLevel=1.4');
+  AProcess.Parameters.Add('-dPDFSETTINGS=/default');
+  AProcess.Parameters.Add('-dEmbedAllFonts=true');
+  AProcess.Parameters.Add('-dAutoRotatePages=/PageByPage');
+  AProcess.Parameters.Add('-dParseDSCComments=false');
+  AProcess.Parameters.Add('-sColorConversionStrategy=RGB');
+  AProcess.Parameters.Add('-dProcessColorModel=/DeviceRGB');
+  AProcess.Parameters.Add('-dConvertCMYKImagesToRGB=true');
+  AProcess.Parameters.Add('-dAutoFilterColorImages=false');
+  AProcess.Parameters.Add('-dAutoFilterGrayImages=false');
+  AProcess.Parameters.Add('-dEncodeColorImages=true');
+  AProcess.Parameters.Add('-dEncodeGrayImages=true');
+  AProcess.Parameters.Add('-dColorImageFilter=/DCTEncode');
+  AProcess.Parameters.Add('-dGrayImageFilter=/DCTEncode');
+  AProcess.Parameters.Add('-dDownsampleColorImages=true');
+  AProcess.Parameters.Add('-dColorImageResolution='+Dpi);
+  AProcess.Parameters.Add('-dDownsampleGrayImages=true');
+  AProcess.Parameters.Add('-dGrayImageResolution='+Dpi);
+  AProcess.Parameters.Add('-dEncodeMonoImages=true');
+  AProcess.Parameters.Add('-dMonoImageFilter=/CCITTFaxEncode');
+  AProcess.Parameters.Add('-c ".setpdfwrite << /ColorImageDict <</QFactor 2.4 /Blend 1 /HSample [2 1 1 2] /VSample [2 1 1 2]>> >> setdistillerparams" ".setpdfwrite << /GrayImageDict <</QFactor 2.4 /Blend 1 /HSample [2 1 1 2] /VSample [2 1 1 2]>> >> setdistillerparams"');
+  AProcess.Parameters.Add('-f');
+  AProcess.Parameters.Add('"' + FileName + '"');
   AProcess.Options := AProcess.Options + [poWaitOnExit, poNoConsole];
   AProcess.Execute;
   while AProcess.Running = true do
   begin
-	  Application.ProcessMessages;
+    Application.ProcessMessages;
   end;
   if OverWrite then
   begin
@@ -108,8 +135,9 @@ begin
     RenameFile(NewFileName, FileName);
   end;
   Application.ProcessMessages;
-  AProcess.Free;
+  FreeAndNil(AProcess);
 end;
+
 
 end.
 
